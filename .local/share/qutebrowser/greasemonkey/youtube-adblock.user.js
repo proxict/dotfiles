a5d7adf851a633eb95681b7756316b27f8223050
proxict <bobogu@protonmail.com>
2023-08-15 22:42:38 +0200

    Updated youtube adblock

--- .local/share/qutebrowser/greasemonkey/youtube-adblock.user.js
+++ .local/share/qutebrowser/greasemonkey/youtube-adblock.user.js
@@ -1,27 +1,28 @@
 // ==UserScript==
-// @name         Auto Skip YouTube Ads 
-// @version      1.0.2
-// @description  Speed up and skip YouTube ads automatically 
-// @author       codiac-killer
+// @name         Auto Skip YouTube Ads
+// @version      1.1.0
+// @description  Speed up and skip YouTube ads automatically
+// @author       jso8910
 // @match        *://*.youtube.com/*
 // @exclude      *://*.youtube.com/subscribe_embed?*
 // ==/UserScript==
+setInterval(() => {
+    const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button')
+    if (btn) {
+        btn.click()
+    }
+    const ad = [...document.querySelectorAll('.ad-showing')][0];
+    if (ad) {
+        const video = document.querySelector('video')
+        video.muted = true;
+        video.hidden = true;
 
-let main = new MutationObserver(() => {
-	// Get skip button and click it
-	let btn = document.getElementsByClassName("ytp-ad-skip-button ytp-button").item(0)
-	if (btn) {
-		btn.click()
-	}
+        // This is not necessarily available right at the start
+        if(video.duration != NaN) {
+            video.currentTime = video.duration;
+        }
 
-	// (unskipable ads) If skip button didn't exist / was not clicked speed up video
-	const ad = [...document.querySelectorAll('.ad-showing')][0];
-	if (ad) {
-		// Speed up and mute
-		document.querySelector('video').playbackRate = 16;
-		document.querySelector('video').muted = true;
-	}
-})
-
-main.observe(document.getElementsByClassName("video-ads ytp-ad-module").item(0), {attributes: true, characterData: true, childList: true})
-
+        // 16 seems to be the highest rate that works, mostly this isn't needed
+        video.playbackRate = 16;
+    }
+}, 50)
